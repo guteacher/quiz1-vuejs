91822fe070e0425d9c03d0c797927fd45b8888a3
Gabriel Ullmann <ullmanng162@gmail.com>
2023-03-31 16:10:42 -0400

    first

---
+++ src/components/CoffeeCardComponent.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="card">
+        Create your card here
+    </div>
+</template>
+
+<script>
+export default {
+  name: "CoffeeCardComponent",
+  props: {
+    addYourPropHere: String
+  }
+};
+</script>
+
+<style scoped>
+    div.card {
+        padding: 5px;
+        border: 1px solid black
+    }
+</style>
